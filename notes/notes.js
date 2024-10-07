04a89cb0d3f30998e9f4798225c65e1cc22be756
Yashraj <yashsharmau9@gmail.com>
2024-10-07 17:19:40 +0530

    Added Notes

---
+++ notes/notes.js
@@ -0,0 +1,39 @@
+/*
+
+THIS IS MADE FOR AMAZING JS NOTES
+
+LET'S FUCKING GO !!!!
+
+*/
+
+
+/*
+
+const array = [];
+
+const type = 1;
+const value = 2;
+
+array.push({type,value});
+
+console.log(array);
+
+Output => 
+
+arr[0] = {type:1, value:2});
+
+// SO THIS BASICALLY STORES as object
+
+array.push([type,value]);
+
+arr[0] = [1,2];
+
+// THIS ONLY STORES VALUES
+
+*/
+
+
+
+
+
+

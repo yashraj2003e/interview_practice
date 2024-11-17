fe5e62b6bc02b2efcad73466fa3391c6e2d17699
Yashraj <yashsharmau9@gmail.com>
2024-11-17 21:54:09 +0530

    random code

--- ts/ts-practice/src/App.tsx
+++ ts/ts-practice/src/App.tsx
@@ -1,14 +1,36 @@
-import { Button, Button1, Button2 } from "./components/Button";
-import CounterContext from "./contexts/CounterContext";
+// import { Button, Button1, Button2 } from "./components/Button";
+// import CounterContext from "./contexts/CounterContext";
+
+import debounce from "debounce";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [val, setVal] = useState<number>(0);
+  useEffect(() => {
+    const scrollPos = debounce(() => {
+      if (window.scrollY > 0) {
+        setVal(0);
+      } else {
+        setVal(1);
+      }
+    });
+
+    window.addEventListener("scroll", scrollPos);
+    return function () {
+      window.removeEventListener("scroll", scrollPos);
+    };
+  }, []);
+
   return (
-    <CounterContext>
-      <div>
-        <Button text={true} />
-        <Button1 text={true} />
-        <Button2 />
-      </div>
-    </CounterContext>
+    // <CounterContext>
+    //   <div>
+    //     <Button text={true} />
+    //     <Button1 text={true} />
+    //     <Button2 />
+    //   </div>
+    // </CounterContext>
+    <div style={{ height: "200vh" }}>
+      <h1 style={{ position: "fixed", top: 0 }}>{val ? "Hello" : "olleH"}</h1>
+    </div>
   );
 }
